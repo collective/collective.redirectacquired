6463472e6fb32875ad5890693404992b6ae03e89
Godefroid Chapelle <gotcha@bubblenet.be>
2018-01-22 11:20:02 +0100

    more focused tests

--- collective/redirectacquired/tests/test_acquisition.py
+++ collective/redirectacquired/tests/test_acquisition.py
@@ -19,8 +19,8 @@ class TestBadAcquisition(unittest.TestCase):
     def assertRequestHasCanonicalURL(self, request, url):
         self.assertEqual(request['CANONICAL_URL'], 'http://nohost' + url)
 
-    def test_page(self):
-        "browsing to acquired page should trigger 301"
+    def test_no_acquisition(self):
+        "no CANONICAL_URL if no acquired content"
         self.portal.invokeFactory('Document', 'a_page')
         self.assertTrue('a_page' in self.portal.objectIds())
         self.portal.invokeFactory('Folder', 'a_folder')
@@ -28,12 +28,19 @@ class TestBadAcquisition(unittest.TestCase):
         request = self.layer['request']
         request.traverse('/plone/a_page')
         self.assertTrue('CANONICAL_URL' not in request)
+
+    def test_no_acquisition(self):
+        "CANONICAL_URL if acquired content"
+        self.portal.invokeFactory('Document', 'a_page')
+        self.assertTrue('a_page' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'a_folder')
+        self.assertTrue('a_folder' in self.portal.objectIds())
+        request = self.layer['request']
         request.traverse('/plone/a_folder/a_page')
         self.assertTrue('CANONICAL_URL' in request)
         self.assertRequestHasCanonicalURL(request, '/plone/a_page/document_view')
-        
     
-    def test_folder(self):
+    def test_folder_default_page(self):
         "browsing to acquired page should trigger 301"
         self.portal.invokeFactory('Folder', 'news')
         self.assertTrue('news' in self.portal.objectIds())
@@ -42,11 +49,46 @@ class TestBadAcquisition(unittest.TestCase):
         self.portal['news'].manage_addProperty('default_page', 'a_page', 'string')
         self.portal.invokeFactory('Folder', 'events')
         self.assertTrue('events' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/events/news')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/news')
+    
+    def test_folder_default_page_with_layout(self):
+        "browsing to acquired page should trigger 301"
+        self.portal.invokeFactory('Folder', 'news')
+        self.assertTrue('news' in self.portal.objectIds())
+        self.portal['news'].invokeFactory('Document', 'a_page')
+        self.assertTrue('a_page' in self.portal['news'].objectIds())
+        self.portal['news'].manage_addProperty('default_page', 'a_page', 'string')
+        self.portal['news']['a_page'].manage_addProperty('layout',
+                'document_view', 'string')
+        self.portal.invokeFactory('Folder', 'events')
+        self.assertTrue('events' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/events/news')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/news')
+
+    def test_folder_edit_alias(self):
+        "browsing to acquired page should trigger 301"
+        self.portal.invokeFactory('Folder', 'news')
+        self.assertTrue('news' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'events')
+        self.assertTrue('events' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/news/events/edit')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/events/edit')
+
+    def test_folder_layout(self):
+        "browsing to acquired page should trigger 301"
+        self.portal.invokeFactory('Folder', 'news')
+        self.assertTrue('news' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'events')
+        self.assertTrue('events' in self.portal.objectIds())
         self.portal['events'].manage_addProperty('layout', 'folder_listing', 'string')
         request = self.layer['request']
         request.traverse('/plone/news/events')
         self.assertTrue('CANONICAL_URL' in request)
         self.assertRequestHasCanonicalURL(request, '/plone/events')
-        request.traverse('/plone/news/events/edit')
-        self.assertTrue('CANONICAL_URL' in request)
-        self.assertRequestHasCanonicalURL(request, '/plone/events/edit')
