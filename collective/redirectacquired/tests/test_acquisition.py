5bd56339daf18ce5062dbccb273da0447c18a655
Godefroid Chapelle <gotcha@bubblenet.be>
2018-01-22 11:40:56 +0100

    more tests
    need to fix the default view case

--- collective/redirectacquired/tests/test_acquisition.py
+++ collective/redirectacquired/tests/test_acquisition.py
@@ -8,6 +8,8 @@ from zope.interface import alsoProvides
 import unittest
 from collective.redirectacquired.testing import BASE_INTEGRATION_TESTING
 
+import pkg_resources
+
 
 class TestBadAcquisition(unittest.TestCase):
 
@@ -29,7 +31,7 @@ class TestBadAcquisition(unittest.TestCase):
         request.traverse('/plone/a_page')
         self.assertTrue('CANONICAL_URL' not in request)
 
-    def test_no_acquisition(self):
+    def test_acquisition(self):
         "CANONICAL_URL if acquired content"
         self.portal.invokeFactory('Document', 'a_page')
         self.assertTrue('a_page' in self.portal.objectIds())
@@ -38,8 +40,47 @@ class TestBadAcquisition(unittest.TestCase):
         request = self.layer['request']
         request.traverse('/plone/a_folder/a_page')
         self.assertTrue('CANONICAL_URL' in request)
-        self.assertRequestHasCanonicalURL(request, '/plone/a_page/document_view')
+        self.assertRequestHasCanonicalURL(request, '/plone/a_page')
     
+    def test_image(self):
+        "CANONICAL_URL if acquired content"
+        self.portal.invokeFactory('Image', 'a_image')
+        self.assertTrue('a_image' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'a_folder')
+        self.assertTrue('a_folder' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/a_folder/a_image')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/a_image')
+
+    def test_image(self):
+        "CANONICAL_URL if acquired content"
+        self.portal.invokeFactory('Image', 'a_image')
+        self.assertTrue('a_image' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'a_folder')
+        self.assertTrue('a_folder' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/a_folder/a_image/view')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/a_image/view')
+
+    def test_image_scale(self):
+        "CANONICAL_URL if acquired content"
+        self.portal.invokeFactory('Image', 'a_image')
+        self.assertTrue('a_image' in self.portal.objectIds())
+        
+        resource_package = 'collective.redirectacquired.tests'  # Could be any module/package name
+        resource_path = '/'.join(('resources', 'sunset.png')) 
+        sunset_png = pkg_resources.resource_stream(resource_package, resource_path)
+        self.portal['a_image'].setImage(sunset_png)
+
+        self.portal.invokeFactory('Folder', 'a_folder')
+        self.assertTrue('a_folder' in self.portal.objectIds())
+        request = self.layer['request']
+        request.traverse('/plone/a_folder/a_image/image_mini')
+        self.assertTrue('CANONICAL_URL' in request)
+        self.assertRequestHasCanonicalURL(request, '/plone/a_image/image_mini')
+
     def test_folder_default_page(self):
         "browsing to acquired page should trigger 301"
         self.portal.invokeFactory('Folder', 'news')
