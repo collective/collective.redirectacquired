22ddd0fa5f97450bd8f01ad71d86fe3a471f02a2
Godefroid Chapelle <gotcha@bubblenet.be>
2018-01-24 15:04:28 +0100

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '1.0a4'
+version = '1.0a5.dev0'
 description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
 setup(
